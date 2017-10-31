9bd364824a45edced6a78f8ed5d9ab737df06d61
satishkurelly <google123>
2017-10-30 21:03:03 -0400

    reg ex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PopoverModule } from 'ngx-bootstrap';
 
-
 import { AppComponent } from './app.component';
 import {PasswordValidatorContainerComponent} from "./directives/pwd-validator/pwd.validator.component";
 import {AccordionModule} from 'primeng/primeng';
-import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -15,9 +14,10 @@ import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-b
   ],
   imports: [
     BrowserModule,
-    PopoverModule.forRoot(),
     AccordionModule,
     BrowserAnimationsModule,
+
+    PopoverModule.forRoot(),
 
 
   ],
